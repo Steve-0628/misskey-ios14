8a9ad6839d29972ee25e8f446a3a5b579275a831
yu256 <122450013+yu256@users.noreply.github.com>
2023-07-26 11:06:20 +0900

    enhance(VRChatAPI): なんかもうめっちゃ変えた
    privateの場合茶色にするように
    トークンがない場合設定へのリンクを表示
    二段階認証が必要な場合アラートを出す
    Webでオンラインのフレンドはバックエンドで除外する
    アイコンをクリックして詳細画面に飛べるように
    トークンの有効期限が切れた場合アラートを出す
    refactor

--- packages/frontend/src/scripts/vrchat-api.ts
+++ packages/frontend/src/scripts/vrchat-api.ts
@@ -1,32 +1,36 @@
 import { defaultStore } from '@/store';
 import { api } from '@/os';
 
-export async function getFriends(): Promise<GetFriendsRes> {
-	return api('vrchat', {
-		requestType: 'getFriends',
+export async function fetchFriends(): Promise<Friend[]> {
+	return api('vrchat/friends', {
 		token: defaultStore.state.VRChatToken,
 	});
 }
 
 export async function fetchToken(user: string, password: string): Promise<FetchTokenRes> {
 	return api('vrchat', {
-		requestType: 'fetchToken',
 		user,
 		password,
 	});
 }
 
-interface FetchTokenRes {
-	response: Response;
+export async function fetchInstance(id: string): Promise<Instance> {
+	return api('vrchat/instance', {
+		token: defaultStore.state.VRChatToken,
+		id,
+	});
 }
 
-interface Response {
+export async function fetchUser(user: string): Promise<User> {
+	return api('vrchat/user', {
+		token: defaultStore.state.VRChatToken,
+		user,
+	});
+}
+
+export interface FetchTokenRes {
 	requiresTwoFactorAuth: string;
 	authToken: string;
-}
-
-interface GetFriendsRes {
-	response: Friend[];
 }
 
 export interface Friend {
@@ -34,4 +38,23 @@ export interface Friend {
 	id: string;
 	location: string;
 	status: string;
+}
+
+export interface Instance {
+	ownerId: string;
+	userCount: number;
+	name: string;
+	description: string;
+	thumbnailImageUrl: string;
+}
+
+export interface User {
+	bio: string;
+	bioLinks: string[];
+	currentAvatarThumbnailImageUrl: string;
+	displayName: string;
+	last_activity: string;
+	location: string;
+	status: string;
+	statusDescription?: string;
 }
