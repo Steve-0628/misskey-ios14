026eac852c74a36ec9dbbc17127c6ba800dba3f5
Steve-0628 <me@stesan.dev>
2025-01-07 02:43:59 +0900

    breaking: remove backend

--- packages/backend/src/misc/check-https.ts
+++
@@ -1,4 +0,0 @@
-export function checkHttps(url: string): boolean {
-	return url.startsWith('https://') ||
-		(url.startsWith('http://') && process.env.NODE_ENV !== 'production');
-}
--- packages/backend/src/misc/is-duplicate-key-value-error.ts
+++
@@ -1,5 +0,0 @@
-import { QueryFailedError } from 'typeorm';
-
-export function isDuplicateKeyValueError(e: unknown | Error): boolean {
-	return e instanceof QueryFailedError && e.driverError.code === '23505';
-}
